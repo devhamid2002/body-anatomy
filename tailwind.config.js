49ead7216d78e4c32e3aaa29294e39f18fac2d42
Hamid Yavarzadeh <yavarzadeh.h@gosafir.com>
2024-11-21 12:24:35 +0330

    Feature: Create Project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {colors: {
+        seafoamGreen: '#9fcdcb',
+        darkseafoamGreen: '#326a69',
+      }},
+  },
+  plugins: [],
+}
